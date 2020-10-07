0f1fb6b085a9e47630e3c2addbe7be2e586baeb0
Max <maxim.nebela@gmail.com>
2020-10-07 19:43:57 +0300

    lol

--- bot.py
+++ bot.py
@@ -129,16 +129,5 @@ def lalala(message):
     except:
         bot.send_message(message.chat.id, "{0.first_name}, введіть /help для допомоги".format(message.from_user, bot.get_me()))
 
-@server.route('/' + TOKEN, methods=['POST'])
-def getMessage():
-    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-9"))])
-    return "!", 200
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://dry-chamber-52499.herokuapp.com/' + TOKEN)
-    return "!", 200
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+if __name__ == '__main__':
+    main()
